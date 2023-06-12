d426c1c5b08498fa090ffe417a3f2ffe61c8bf50
bysxiang <bysxrokr@qq.com>
2023-06-12 12:33:15 +0800

    添加GetWorkbook方法。

--- Helpers/UipathExcelHelper.cs
+++ Helpers/UipathExcelHelper.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using ClosedXML;
+using DocumentFormat.OpenXml.InkML;
+using UiPath.Excel;
 using UiPath.Excel.Activities;
 
 namespace Bysxiang.UipathExcelEx.Helpers
@@ -20,6 +24,34 @@ namespace Bysxiang.UipathExcelEx.Helpers
             Version v = t.Assembly.GetName().Version;
 
             return v.MajorRevision == 2 && v.Minor < 10;
+        }
+
+        public static WorkbookApplication GetWorkbook(AsyncCodeActivityContext context)
+        {
+            Assembly ass = typeof(ExcelApplicationScope).Assembly;
+            Type t = ass.GetType("UiPath.Excel.Activities.WorkflowDataContextExtensions");
+            if (t == null)
+            {
+                return context.DataContext.GetProperties()[UipathExcelHelper.GetWorkbookScopePropertyTag()].GetValue(context.DataContext) as WorkbookApplication;
+            }
+            else
+            {
+                MethodInfo methodInfo = t.GetMethod("GetWorkbookApplication");
+                object w = methodInfo.Invoke(null, new object[] {context.DataContext});
+                if (w is WorkbookApplication)
+                {
+                    return w as WorkbookApplication;
+                }
+                else if (w is WorkbookQuickHandle)
+                {
+                    IQuickHandleParent parent = (IQuickHandleParent)w;
+                    return parent.GetWorkbook() as WorkbookApplication;
+                }
+                else
+                {
+                    throw new Exception("无法获取WorkbookApplication");
+                }
+            }
         }
 
         public static string GetWorkbookScopePropertyTag()
