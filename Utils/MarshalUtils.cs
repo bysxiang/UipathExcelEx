04a0699f0796c0bd2f57e7731a1729e674af350f
bysxiang <bysxrokr@qq.com>
2023-07-04 11:22:02 +0800

    添加MarshalUtils类。

---
+++ Utils/MarshalUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bysxiang.UipathExcelEx.Utils
+{
+    public static class MarshalUtils
+    {
+        public static void ReleaseComObject<T>(ref T o) where T : class
+        {
+            if (o != null && Marshal.IsComObject(o))
+            {
+                int count = Marshal.ReleaseComObject(o);
+            }
+        }
+    }
+}
