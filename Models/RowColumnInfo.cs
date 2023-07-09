c2e7913c5847e9c9547142505809604cd1d9283e
bysxiang <bysxrokr@qq.com>
2023-07-09 09:51:48 +0800

    对Value、Text属性添加set

--- Models/RowColumnInfo.cs
+++ Models/RowColumnInfo.cs
@@ -17,9 +17,9 @@ namespace Bysxiang.UipathExcelEx.Models
 
         public CellPosition EndPosition { get; }
 
-        public object Value { get; }
+        public object Value { get; set; }
 
-        public string Text { get; }
+        public string Text { get; set; }
 
         public Color BackgroundColor { get; }
 
